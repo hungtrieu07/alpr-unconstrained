a53924bfe3825c747ed760eb61c538bc7242bd64
hungtrieu07 <hungtrieu0701@gmail.com>
2022-10-20 13:52:41 +0700

    fix

--- license-plate-detection.py
+++ license-plate-detection.py
@@ -3,13 +3,14 @@ import keras
 import cv2
 import traceback
 
-from src.keras_utils			import load_model, detect_lp
-from src.utils					import im2single
+from src.keras_utils 			import load_model
 from glob 						import glob
 from os.path 					import splitext, basename
+from src.utils 					import im2single
+from src.keras_utils 			import load_model, detect_lp
 from src.label 					import Shape, writeShapes
-
-
+from time import sleep
+import datetime
 def adjust_pts(pts,lroi):
 	return pts*lroi.wh().reshape((2,1)) + lroi.tl().reshape((2,1))
 
@@ -17,11 +18,11 @@ def adjust_pts(pts,lroi):
 if __name__ == '__main__':
 
 	try:
+		
 		input_dir  = sys.argv[1]
 		output_dir = input_dir+"/output"
 		if not os.path.exists(output_dir):
-			os.mkdir(output_dir)
-
+			os.mkdir(output_dir)		
 		lp_threshold = .5
 
 		wpod_net_path = sys.argv[2]
@@ -29,21 +30,21 @@ if __name__ == '__main__':
 
 		imgs_paths = glob('%s/*.png' % input_dir)
 
-		print('Searching for license plates using WPOD-NET')
+		print ('Searching for license plates using WPOD-NET')
 
 		for i,img_path in enumerate(imgs_paths):
 
-			print(('\t Processing %s' % img_path))
-
+			print ('\t Processing %s' % img_path)
+			start = datetime.datetime.now()
 			bname = splitext(basename(img_path))[0]
 			Ivehicle = cv2.imread(img_path)
 
 			ratio = float(max(Ivehicle.shape[:2]))/min(Ivehicle.shape[:2])
-			side  = int(ratio * 288.)
-			bound_dim = min(side + (side%(2**4)), 608)
-			print(("\t\tBound dim: %d, ratio: %f" % (bound_dim,ratio)))
+			side  = int(ratio*288.)
+			bound_dim = min(side + (side%(2**4)),608)
+			print( "\t\tBound dim: %d, ratio: %f" % (bound_dim,ratio))
 
-			Llp,LlpImgs,_, = detect_lp(wpod_net,im2single(Ivehicle),bound_dim,2**4,(160,160),lp_threshold)
+			Llp,LlpImgs,_ = detect_lp(wpod_net,im2single(Ivehicle),bound_dim,2**4,(240,80),lp_threshold)
 
 			if len(LlpImgs):
 				Ilp = LlpImgs[0]
@@ -54,9 +55,13 @@ if __name__ == '__main__':
 
 				cv2.imwrite('%s/%s_lp.png' % (output_dir,bname),Ilp*255.)
 				writeShapes('%s/%s_lp.txt' % (output_dir,bname),[s])
+			stop = datetime.datetime.now()
+			print(stop-start)
 
 	except:
 		traceback.print_exc()
 		sys.exit(1)
-
+	sleep(10)
 	sys.exit(0)
+
+
--- train-detector.py
+++ train-detector.py
@@ -1,10 +1,10 @@
+
 import sys
 import numpy as np
 import cv2
 import argparse
 import keras
-import tensorflow as tf
-
+import tensorflow
 from random import choice
 from os.path import isfile, isdir, basename, splitext
 from os import makedirs
@@ -48,131 +48,81 @@ def process_data_item(data_item,dim,model_stride):
 
 
 if __name__ == '__main__':
-    config = tf.compat.v1.ConfigProto( device_count = {'GPU': 2 , 'CPU': 16} ) 
-    config.gpu_options.allow_growth = True
-    sess = tf.compat.v1.Session(config=config) 
-    tf.compat.v1.keras.backend.set_session(sess)
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m' 		,'--model'			,type=str   , required=True		,help='Path to previous model')
-    parser.add_argument('-n' 		,'--name'			,type=str   , required=True		,help='Model name')
-    parser.add_argument('-tr'		,'--train-dir'		,type=str   , required=True		,help='Input data directory for training')
-    parser.add_argument('-te'		,'--test-dir'		,type=str   , required=True		,help='Input data directory for testing')
-    parser.add_argument('-its'		,'--iterations'		,type=int   , default=300000	,help='Number of mini-batch iterations (default = 300.000)')
-    parser.add_argument('-bs'		,'--batch-size'		,type=int   , default=32		,help='Mini-batch size (default = 32)')
-    parser.add_argument('-od'		,'--output-dir'		,type=str   , default='./'		,help='Output directory (default = ./)')
-    parser.add_argument('-op'		,'--optimizer'		,type=str   , default='Adam'	,help='Optmizer (default = Adam)')
-    parser.add_argument('-lr'		,'--learning-rate'	,type=float , default=.01		,help='Optmizer (default = 0.01)')
-    args = parser.parse_args()
+	config = tensorflow.compat.v1.ConfigProto( device_count = {'GPU': 2 , 'CPU': 16} ) 
+	config.gpu_options.allow_growth = True
+	sess = tensorflow.compat.v1.Session(config=config) 
+	tensorflow.compat.v1.keras.backend.set_session(sess)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-m' 		,'--model'			,type=str   , required=True		,help='Path to previous model')
+	parser.add_argument('-n' 		,'--name'			,type=str   , required=True		,help='Model name')
+	parser.add_argument('-tr'		,'--train-dir'		,type=str   , required=True		,help='Input data directory for training')
+	parser.add_argument('-its'		,'--iterations'		,type=int   , default=300000	,help='Number of mini-batch iterations (default = 300.000)')
+	parser.add_argument('-bs'		,'--batch-size'		,type=int   , default=32		,help='Mini-batch size (default = 32)')
+	parser.add_argument('-od'		,'--output-dir'		,type=str   , default='./'		,help='Output directory (default = ./)')
+	parser.add_argument('-op'		,'--optimizer'		,type=str   , default='Adam'	,help='Optmizer (default = Adam)')
+	parser.add_argument('-lr'		,'--learning-rate'	,type=float , default=.01		,help='Optmizer (default = 0.01)')
+	args = parser.parse_args()
 
-    netname 	= basename(args.name)
-    train_dir 	= args.train_dir
-    test_dir    = args.test_dir
-    outdir 		= args.output_dir
+	netname 	= basename(args.name)
+	train_dir 	= args.train_dir
+	outdir 		= args.output_dir
 
-    iterations 	= args.iterations
-    batch_size 	= args.batch_size
-    dim 		= 208
+	iterations 	= args.iterations
+	batch_size 	= args.batch_size
+	dim 		= 208
 
-    if not isdir(outdir):
-        makedirs(outdir)
+	if not isdir(outdir):
+		makedirs(outdir)
 
-    model,model_stride,xshape,yshape = load_network(args.model,dim)
+	model,model_stride,xshape,yshape = load_network(args.model,dim)
 
-    opt = getattr(tf.keras.optimizers,args.optimizer)(lr=args.learning_rate)
-    model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
+	opt = getattr(keras.optimizers,args.optimizer)(lr=args.learning_rate)
+	model.compile(loss=loss, optimizer=opt)
 
-    tb = tf.keras.callbacks.TensorBoard(log_dir='Logs/Exp_15', 
-                                    histogram_freq=0, 
-                                    batch_size=batch_size, 
-                                    write_graph=True, 
-                                    write_grads=False)
-    tb.set_model(model)
+	print('Checking input directory...')
+	Files = image_files_from_folder(train_dir)
 
-    print('Checking input directory...')
-    Files_train = image_files_from_folder(train_dir)
-    Files_test = image_files_from_folder(test_dir)
+	Data = []
+	for file in Files:
+		labfile = splitext(file)[0] + '.txt'
+		if isfile(labfile):
+			L = readShapes(labfile)
+			I = cv2.imread(file)
+			Data.append([I,L[0]])
 
-    Data_train = []
-    for file in Files_train:
-        labfile = splitext(file)[0] + '.txt'
-        if isfile(labfile):
-            L = readShapes(labfile)
-            I = cv2.imread(file)
-            Data_train.append([I,L[0]])
+	print('%d images with labels found' % len(Data))
 
-    print(('%d train images with labels found' % len(Data_train)))
-
-    Data_test = []
-    for file in Files_test:
-        labfile = splitext(file)[0] + '.txt'
-        if isfile(labfile):
-            L = readShapes(labfile)
-            I = cv2.imread(file)
-            Data_test.append([I,L[0]])
-
-    print(('%d test images with labels found' % len(Data_test)))
-
-    dg1 = DataGenerator(	data=Data_train, \
+	dg = DataGenerator(	data=Data, \
 						process_data_item_func=lambda x: process_data_item(x,dim,model_stride),\
 						xshape=xshape, \
 						yshape=(yshape[0],yshape[1],yshape[2]+1), \
 						nthreads=2, \
 						pool_size=1000, \
 						min_nsamples=100 )
-    dg1.start()
+	dg.start()
 
-    dg2 = DataGenerator(	data=Data_test, \
-						process_data_item_func=lambda x: process_data_item(x,dim,model_stride),\
-						xshape=xshape, \
-						yshape=(yshape[0],yshape[1],yshape[2]+1), \
-						nthreads=2, \
-						pool_size=1000, \
-						min_nsamples=100 )
-    dg2.start()
+	Xtrain = np.empty((batch_size,dim,dim,3),dtype='single')
+	Ytrain = np.empty( ( int(batch_size), int(dim/model_stride), int(dim/model_stride), int(2*4+1) ))
 
-    Xtrain = np.empty((batch_size,dim,dim,3),dtype='single')
-    Ytrain = np.empty( ( int(batch_size), int(dim/model_stride), int(dim/model_stride), int(2*4+1) ))
+	model_path_backup = '%s/%s_backup' % (outdir,netname)
+	model_path_final  = '%s/%s_final'  % (outdir,netname)
 
-    Xtest = np.empty((batch_size,dim,dim,3),dtype='single')
-    Ytest = np.empty( ( int(batch_size), int(dim/model_stride), int(dim/model_stride), int(2*4+1) ))
-	
-    model_path_backup = '%s/%s_backup' % (outdir,netname)
-    model_path_final  = '%s/%s_final'  % (outdir,netname)
-	
-	# history = model.fit(Xtrain, Ytrain, batch_size=batch_size, epochs=10000)
+	for it in range(iterations):
 
-    for it in range(iterations):
-        train_accuracy = []
-        train_loss = []
-        test_accuracy = []
-        test_loss = []
-        print(('Iter. %d (of %d)' % (it+1,iterations)))
+		print('Iter. %d (of %d)' % (it+1,iterations))
 
-        Xtrain,Ytrain = dg1.get_batch(batch_size)
-        Xtest,Ytest = dg2.get_batch(batch_size)
+		Xtrain,Ytrain = dg.get_batch(batch_size)
+		train_loss = model.train_on_batch(Xtrain,Ytrain)
 
-        tr_loss, tr_acc = model.train_on_batch(Xtrain,Ytrain)
-        train_accuracy.append(tr_acc)
-        train_loss.append(tr_loss)
-
-        te_loss, te_acc = model.test_on_batch(Xtest, Ytest)
-        test_accuracy.append(te_acc)
-        test_loss.append(te_acc)
-
-        tb.on_epoch_end(it, {'train_loss': tr_loss, 'train_acc': tr_acc})
-        tb.on_epoch_end(it, {'test_loss': te_loss, 'test_acc': te_acc})
-
-        print('epoch', it, 'batch', it, 'train_loss:', tr_loss, 'train_acc:', tr_acc)
-        print('epoch', it, 'batch', it, 'test_loss:', te_loss, 'test_acc:', te_acc)
+		print('\tLoss: %f' % train_loss)
 
 		# Save model every 1000 iterations
-        if (it+1) % 1000 == 0:
-            print(('Saving model (%s)' % model_path_backup))
-            save_model(model,model_path_backup)
-    tb.on_train_end(None)
-    print('Stopping data train generator')
-    dg1.stop()
-    dg2.stop()
+		if (it+1) % 1000 == 0:
+			print('Saving model (%s)' % model_path_backup)
+			save_model(model,model_path_backup)
 
-    print(('Saving model (%s)' % model_path_final))
-    save_model(model,model_path_final)
+	print('Stopping data generator')
+	dg.stop()
+
+	print('Saving model (%s)' % model_path_final)
+	save_model(model,model_path_final)
